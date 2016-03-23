3b26dd25f44d06fdd61ac6419ef7329f4605aba3
Kristoffer Walker <kris@kixx.name>
2016-03-23 05:54:51 -0400

    Start of Inprocess RequestChannel tests.
    	new file:   spec/inprocess_transport/request_channel_spec.js

---
+++ spec/inprocess_transport/request_channel_spec.js
@@ -0,0 +1,52 @@
+/* global describe, beforeAll, afterEach, it, expect */
+/* eslint-disable max-nested-callbacks */
+'use strict';
+
+var oddcast = require('../../lib/oddcast');
+
+describe('InprocessTransport with CommandChannel', function () {
+	var PAYLOAD_1 = Object.freeze({event: 1});
+	var RESPONSE_1 = Object.freeze({response: 1});
+	// var ERROR_1 = new Error('ERROR_1');
+
+	beforeAll(function () {
+		this.channel = oddcast.requestChannel();
+		this.transport = oddcast.inprocessTransport();
+		this.channel.use({role: 'foo'}, this.transport);
+	});
+
+	afterEach(function () {
+		this.channel.removeAllListeners('error');
+		this.channel.remove({});
+	});
+
+	it('calls handlers asynchronously', function (done) {
+		var async = false;
+
+		this.channel.respond({role: 'foo'}, function () {
+			expect(async).toBe(true);
+			return RESPONSE_1;
+		});
+
+		this.channel.request({role: 'foo'}, PAYLOAD_1)
+			.then(function () {
+				done();
+			});
+
+		async = true;
+	});
+
+	it('passes the payload to handlers', function (done) {
+		this.channel.respond({role: 'foo'}, function (payload) {
+			expect(payload).toBe(PAYLOAD_1);
+			done();
+		});
+
+		this.channel.request({role: 'foo'}, PAYLOAD_1);
+	});
+
+	it('removes all handlers (in afterEach())', function () {
+		var matcher = this.channel.handlerMatcher;
+		expect(Object.keys(matcher.index).length).toBe(0);
+	});
+});
