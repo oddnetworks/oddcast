3ed74a181413823c76d3b5c2a71a6efe6efea8d5
Kristoffer Walker <kris@kixx.name>
2016-03-23 15:01:28 -0400

    Broken tests on bus commands.
    	modified:   lib/bus.js
    	modified:   spec/bus_spec.js

--- spec/bus_spec.js
+++ spec/bus_spec.js
@@ -10,6 +10,7 @@ describe('Bus', function () {
 		PATTERN1: {foo: 'bar'},
 		HANDLER1: function () {},
 		ARGS1: {ARGS: 1},
+		PAYLOAD1: {PAYLOAD: 1},
 		RES1: {RES: 1},
 		RES2: {RES: 2},
 		ERROR1: new Error('TEST_ERROR_1')
@@ -47,10 +48,6 @@ describe('Bus', function () {
 
 	describe('broadcast', function () {
 		var RES1 = 'UNDEFINED';
-		var CONST = Object.freeze({
-			PATTERN1: {foo: 'bar'},
-			PAYLOAD1: {PAYLOAD1: 1}
-		});
 
 		beforeAll(function () {
 			this.bus = createBus();
@@ -68,6 +65,136 @@ describe('Bus', function () {
 
 		it('returns itself', function () {
 			expect(RES1).toBe(this.bus);
+		});
+	});
+
+	describe('commands', function () {
+		describe('handler returns non promise with no callback', function () {
+			var ERROR1 = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function () {
+					return undefined;
+				});
+				this.bus.sendCommand({foo: 'bar'}, CONST.ARGS1).then(done, function (err) {
+					ERROR1 = err;
+					done();
+				});
+			});
+
+			it('rejects with an Error', function () {
+				expect(ERROR1.message).toBe(
+					'A Promise is expected from a queryHandler when no callback is supplied');
+			});
+		});
+
+		describe('handler returns a promise and has a callback', function () {
+			var RES = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function (args, next) {
+					next(null, CONST.RES1);
+					return Promise.resolve(CONST.RES2);
+				});
+				this.bus.sendCommand({foo: 'bar'}, CONST.ARGS1).then(function (res) {
+					RES = res;
+					done();
+				}, done.fail);
+			});
+
+			it('returns the value passed to the callback', function () {
+				expect(RES).toBe(CONST.RES1);
+			});
+		});
+
+		describe('handler returns promise', function () {
+			var RES = 'UNDEFINED';
+			var ARGS = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function (args) {
+					ARGS = args;
+					return Promise.resolve(CONST.RES1);
+				});
+				this.bus.command({foo: 'bar'}, CONST.ARGS1).then(function (res) {
+					RES = res;
+					done();
+				}, done.fail);
+			});
+
+			it('returns the value passed to the callback', function () {
+				expect(RES).toBe(CONST.RES1);
+			});
+
+			it('passes the given arguments Object', function () {
+				expect(ARGS).toEqual(CONST.ARGS1);
+			});
+		});
+
+		describe('handler returns rejected promise', function () {
+			var ERROR = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function () {
+					return Promise.reject(CONST.ERROR1);
+				});
+				this.bus.command({foo: 'bar'}, CONST.ARGS1).then(done, function (err) {
+					ERROR = err;
+					done();
+				});
+			});
+
+			it('rejects with the rejected error', function () {
+				expect(ERROR).toBe(CONST.ERROR1);
+			});
+		});
+
+		describe('handler calls callback', function () {
+			var RES = 'UNDEFINED';
+			var ARGS = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function (args, next) {
+					ARGS = args;
+					next(null, CONST.RES1);
+				});
+				this.bus.sendCommand({foo: 'bar'}, CONST.ARGS1).then(function (res) {
+					RES = res;
+					done();
+				}, done.fail);
+			});
+
+			it('returns the value passed to the callback', function () {
+				expect(RES).toBe(CONST.RES1);
+			});
+
+			it('passes the given arguments Object', function () {
+				expect(ARGS).toEqual(CONST.ARGS1);
+			});
+		});
+
+		describe('handler calls callback with error', function () {
+			var ERROR = 'UNDEFINED';
+
+			beforeAll(function (done) {
+				this.bus = createBus();
+				this.bus.commandHandler({foo: 'bar'}, function (args, next) {
+					next(CONST.ERROR1);
+				});
+				this.bus.sendCommand({foo: 'bar'}, CONST.ARGS1).then(done, function (err) {
+					ERROR = err;
+					done();
+				});
+			});
+
+			it('rejects with the passed error', function () {
+				expect(ERROR).toBe(CONST.ERROR1);
+			});
 		});
 	});
 
